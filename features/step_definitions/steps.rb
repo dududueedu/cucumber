687b5270b5281b4427714129e8462ba0acff4908
dududueedu <eduardooffside@gmail.com>
2021-11-22 16:33:26 -0300

    add-steps

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Dado("que hoje é {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("pergunto qual é a Promoção do dia") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("a resposta deve ser {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
